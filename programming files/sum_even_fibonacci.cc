a72ae34039440b0798073b80d024ca94c2b06c92
Ashley <112506844+ashleyk26@users.noreply.github.com>
2024-04-11 10:37:37 -0400

    Update sum_even_fibonacci.cc

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -2,15 +2,19 @@
 
 int main()
 {
-    int a = 0, b = 1;
+    //changed line 6 AK
+    int a = 0, b = 1.0;
     int sum_even = 0;
     while (b < 4000000) {
         if (b % 2 == 0) {
             // Adds b to sum_even when b is even
             sum_even += b;
         }
+        //changed - AK
+        int c = b
         int temp = b;
-        b = a + b;
+        //changed - AK
+        b = a + b - c;
         a = temp;
     }
 
