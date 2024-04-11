c2ba108dd7a303672920828483113381754f8f0f
Ashley <112506844+ashleyk26@users.noreply.github.com>
2024-04-11 10:26:46 -0400

    Update even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -2,8 +2,10 @@ with open("numbers.html", "w") as f:
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
     for i in range(1, 50):
+        #this is checking if the number is even
         if i % 2 == 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
+            #here is the odd number section
         else:
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
     f.write("</table>\n</body>\n</html>")
